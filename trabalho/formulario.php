cab6d4f0481961204d608a1352302e9c8ed7d64c
Davi Farias <128735645+dawiexper@users.noreply.github.com>
2023-06-06 23:15:26 -0300

    Update formulario.php

--- trabalho/formulario.php
+++ trabalho/formulario.php
@@ -96,7 +96,7 @@
     </style>
 </head>
 <body>
-    <a href="home.php">Voltar</a>
+    <a href="index.php">Voltar</a>
     <div class="box">
         <form action="formulario.php" method="POST" onsubmit="return validateForm()">
             <fieldset>
